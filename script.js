14f7f63d9e968ad7fb8165a5ff7dfdef3a42d027
Kalpesh Doere <99384935+Kalpesh50@users.noreply.github.com>
2023-01-16 19:38:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1ef3277556msh830a11867df0438p1343f2jsn9c586778b23a',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeather = (city) =>{
+	cityName.innerHTML = city
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+	.then(response => response.json())
+	.then((response) => {
+
+
+		console.log(response)
+		
+		temp.innerHTML = response.temp
+		
+		humidity.innerHTML = response.humidity
+		
+		wind_speed.innerHTML = response.wind_speed
+		
+
+
+
+
+
+	})
+	.catch(err => console.error(err));
+}
+submit.addEventListener("click", (e)=>{
+	e.preventDefault()
+	getWeather(city.value)
+})
+
+getWeather("Delhi")
+
+
+
